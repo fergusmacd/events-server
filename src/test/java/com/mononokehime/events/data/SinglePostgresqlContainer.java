a528a5685fdec34de8bd2dd4e0970c9588ef70bf
Fergus MacDermot <fergusmacdermot@gmail.com>
2020-01-29 19:55:13 +0800

    Adding events files

---
+++ src/test/java/com/mononokehime/events/data/SinglePostgresqlContainer.java
@@ -0,0 +1,52 @@
+package com.mononokehime.events.data;
+
+/*-
+ * #%L
+ * Events Server Application
+ * %%
+ * Copyright (C) 2020 Mononokehime
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public class SinglePostgresqlContainer extends PostgreSQLContainer<SinglePostgresqlContainer> {
+    private static final String IMAGE_VERSION = "postgres:11.1";
+    private static SinglePostgresqlContainer container;
+
+    private SinglePostgresqlContainer() {
+        super(IMAGE_VERSION);
+    }
+
+    public static SinglePostgresqlContainer getInstance() {
+        if (container == null) {
+            container = new SinglePostgresqlContainer();
+        }
+        return container;
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        System.setProperty("DB_URL", container.getJdbcUrl());
+        System.setProperty("DB_USERNAME", container.getUsername());
+        System.setProperty("DB_PASSWORD", container.getPassword());
+    }
+
+    @Override
+    public void stop() {
+        //do nothing, JVM handles shut down
+    }
+}
