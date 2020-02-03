74dd94b804fa1af26f5df1e23be94d729d6b9ad3
Fergus MacDermot <fergusmacdermot@gmail.com>
2020-02-03 23:42:35 +0800

    Adding event objects and tests

---
+++ src/main/java/com/mononokehime/events/common/LocaleDateFormatter.java
@@ -0,0 +1,34 @@
+package com.mononokehime.events.common;
+
+/*-
+ * #%L
+ * Events Server Application
+ * %%
+ * Copyright (C) 2020 Mononokehime
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
+
+public class LocaleDateFormatter {
+    private LocaleDateFormatter() {
+    }
+
+    private static final DateTimeFormatter DATETIME_FORMATTER = ISODateTimeFormat.dateTime();
+    public static DateTimeFormatter getDateTimeFormatter() {
+        return DATETIME_FORMATTER;
+    }
+}
---
+++ src/main/java/com/mononokehime/events/model/EventDTO.java
@@ -0,0 +1,54 @@
+package com.mononokehime.events.model;
+
+/*-
+ * #%L
+ * Events Server Application
+ * %%
+ * Copyright (C) 2020 Mononokehime
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+import org.hibernate.validator.constraints.URL;
+import org.joda.time.DateTime;
+import org.springframework.hateoas.RepresentationModel;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@ToString
+@EqualsAndHashCode(exclude = {"id"})
+public class EventDTO extends RepresentationModel<EventDTO> {
+    private @Id
+    @GeneratedValue
+    UUID id;
+
+    private String venue;
+    private DateTime startDateTime;
+    @URL
+    private String bookingURL;
+    private String name;
+    private String description;
+    private String countryCode;
+
+}
